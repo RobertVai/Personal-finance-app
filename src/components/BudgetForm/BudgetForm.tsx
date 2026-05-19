8f3fc6e73e7feb7e8767e5cb3e019263c889424e
RobertVai <vaitechovskij@gmail.com>
2026-05-19 14:53:14 +0300

    feat: implement add and edit budget functionality

--- src/components/BudgetForm/BudgetForm.tsx
+++ src/components/BudgetForm/BudgetForm.tsx
@@ -1,8 +1,74 @@
+import { useEffect, useState } from "react";
 import styles from "./BudgetForm.module.css";
+import type { Budget } from "../../types/budget";
 
-const BudgetForm = () => {
+type BudgetFormProps = {
+  setBudgets: React.Dispatch<React.SetStateAction<Budget[]>>;
+  setIsBudgetModalOpen: (value: boolean) => void;
+  editingBudget: Budget | null;
+  setEditingBudget: (value: Budget | null) => void;
+};
+
+const BudgetForm = ({
+  setBudgets,
+  setIsBudgetModalOpen,
+  editingBudget,
+  setEditingBudget,
+}: BudgetFormProps) => {
+  const [category, setCategory] = useState("");
+  const [maximum, setMaximum] = useState("");
+  const [theme, setTheme] = useState("");
+
+  const handleAddBudget = (e: React.FormEvent) => {
+    e.preventDefault();
+
+    if (editingBudget) {
+      setBudgets((prev) =>
+        prev.map((budget) =>
+          budget.id === editingBudget.id
+            ? {
+                ...budget,
+                category,
+                maximum: Number(maximum),
+                theme,
+              }
+            : budget,
+        ),
+      );
+      setEditingBudget(null);
+      setIsBudgetModalOpen(false);
+
+      return;
+    }
+
+    if (!category.trim() || !maximum.trim() || !theme.trim()) {
+      return;
+    }
+
+    const newBudget = {
+      id: Date.now(),
+      category,
+      maximum: Number(maximum),
+      theme,
+    };
+
+    setBudgets((prev) => [...prev, newBudget]);
+    setCategory("");
+    setMaximum("");
+    setTheme("");
+    setIsBudgetModalOpen(false);
+  };
+
+  useEffect(() => {
+    if (editingBudget) {
+      setCategory(editingBudget.category);
+      setMaximum(editingBudget.maximum.toString());
+      setTheme(editingBudget.theme);
+    }
+  }, [editingBudget]);
+
   return (
-    <form className={styles.form}>
+    <form className={styles.form} onSubmit={handleAddBudget}>
       <p className={styles.description}>
         Choose a category to set a spending budget. These categories can help
         you monitor spending.
@@ -11,11 +77,24 @@ const BudgetForm = () => {
       <div className={styles["form-group"]}>
         <label htmlFor="budget-category">Budget Category</label>
 
-        <select id="budget-category" className={styles.input}>
-          <option>Entertainment</option>
-          <option>Bills</option>
-          <option>Dining Out</option>
-          <option>Personal Care</option>
+        <select
+          id="budget-category"
+          className={styles.input}
+          value={category}
+          onChange={(e) => setCategory(e.target.value)}
+        >
+          <option value="">Select category</option>
+          {editingBudget && (
+            <option value={editingBudget.category}>
+              {editingBudget.category}
+            </option>
+          )}
+
+          <option value="Investments">Investments</option>
+          <option value="Gifts">Gifts</option>
+          <option value="Hobbies">Hobbies</option>
+          <option value="Fitness">Fitness</option>
+          <option value="Subscriptions">Subscriptions</option>
         </select>
       </div>
 
@@ -24,12 +103,13 @@ const BudgetForm = () => {
 
         <div className={styles["amount-input-wrapper"]}>
           <span>$</span>
-
           <input
             id="maximum-spend"
             type="number"
             placeholder="e.g. 2000"
             className={styles["amount-input"]}
+            value={maximum}
+            onChange={(e) => setMaximum(e.target.value)}
           />
         </div>
       </div>
@@ -37,16 +117,22 @@ const BudgetForm = () => {
       <div className={styles["form-group"]}>
         <label htmlFor="theme">Theme</label>
 
-        <select id="theme" className={styles.input}>
-          <option>Green</option>
-          <option>Cyan</option>
-          <option>Yellow</option>
-          <option>Pink</option>
+        <select
+          id="theme"
+          className={styles.input}
+          value={theme}
+          onChange={(e) => setTheme(e.target.value)}
+        >
+          <option value="">Select theme</option>
+          <option value="#277C78">Green</option>
+          <option value="#82C9D7">Cyan</option>
+          <option value="#F2CDAC">Yellow</option>
+          <option value="#626070">Navy</option>
         </select>
       </div>
 
       <button type="submit" className={styles["submit-button"]}>
-        Add Budget
+        {editingBudget ? "Save Changes" : "Add Budget"}
       </button>
     </form>
   );
--- src/pages/BudgetsPage/BudgetsPage.tsx
+++ src/pages/BudgetsPage/BudgetsPage.tsx
@@ -6,11 +6,14 @@ import { useState } from "react";
 import Modal from "../../components/Modal/Modal";
 import BudgetForm from "../../components/BudgetForm/BudgetForm";
 import ActionsMenu from "../../components/ActionsMenu/ActionsMenu";
+import type { Budget } from "../../types/budget";
 
 const BudgetsPage = () => {
   const [budgets, setBudgets] = useState(financeData.budgets);
   const [isBudgetModalOpen, setIsBudgetModalOpen] = useState(false);
   const [openMenuId, setOpenMenuId] = useState<string | null>(null);
+  const [editingBudget, setEditingBudget] = useState<Budget | null>(null);
+
   const budgetsWithTransactions = budgets.map((budget) => {
     const recentSpendings = financeData.transactions
       .filter((transaction) => transaction.category === budget.category)
@@ -69,8 +72,11 @@ const BudgetsPage = () => {
               <ActionsMenu
                 editLabel="Edit Budget"
                 deleteLabel="Delete Budget"
-                onEdit={() => console.log("edit")}
-                onDelete={() => console.log("delete")}
+                onEdit={() => {
+                  setEditingBudget(budget);
+                  setIsBudgetModalOpen(true);
+                }}
+                onDelete={() => deleteBudget(budget.id)}
               />
             )}
             <p>Maximum of {budget.maximum}</p>
@@ -93,9 +99,14 @@ const BudgetsPage = () => {
       <Modal
         isOpen={isBudgetModalOpen}
         onClose={() => setIsBudgetModalOpen(false)}
-        title="Add New Budget"
+        title={editingBudget ? "Edit Budget" : "Add New Budget"}
       >
-        <BudgetForm />
+        <BudgetForm
+          setBudgets={setBudgets}
+          setIsBudgetModalOpen={setIsBudgetModalOpen}
+          editingBudget={editingBudget}
+          setEditingBudget={setEditingBudget}
+        />
       </Modal>
     </div>
   );
